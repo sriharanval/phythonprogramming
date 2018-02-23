7762d33c9369c998b53190736d0a93be08a58a81
sriharanval <36755406+sriharanval@users.noreply.github.com>
2018-02-23 14:52:25 +0530

    Update check the number is positive or negative or zero.py

--- Beginner/check the number is positive or negative or zero.py
+++ Beginner/check the number is positive or negative or zero.py
@@ -3,7 +3,7 @@ if (1<=num <=100000):
     print("positive number");
 elif(-1>=num >=-100000):
     print("negative number");
-elif ( num==0):
+elif(num==0):
     print("zero");
 else:
         print("the number exceed the limit.")
